df95a8dca01e8e3f6261ac6cd94af96f5c6d028b
Eduardo <eduardoromeiro_@outlook.com>
2023-03-07 19:33:12 -0300

    Aprendendo os fundamentos

---
+++ fundamentos/primeiro_programa.py
@@ -0,0 +1,4 @@
+print('Primeiro programa!')
+print('Aprendendo Python com Leonardo Leitão da COD3R')
+print('Isso é uma sentença de código!')
+print('Isso é uma outra sentença de código!')
---
+++ fundamentos/tipos.py
@@ -0,0 +1,10 @@
+print(True)  # Boolean
+print(False)
+print(1.2 + 1)  # Float
+print("Isso é uma string!")
+print('Isso é uma string!')  # String
+print('Você é ' + 3 * 'muito ' + 'legal!')
+# print(3 + '3') -> Ambiguidade
+print([1, 2, 3])  # Lista
+print({'nome': 'Pedro', 'idade': 30})  # Dicionário
+print(None)
